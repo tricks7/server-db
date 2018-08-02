20b4e7f414ace71036b888ded748134854459848
tricks7 <tricks7@me.com>
2018-08-02 11:11:36 -0600

    changed port variable

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const queries = require('./queries')
-const port = 7000 || process.env.NODE_ENV
+const port = process.env.NODE_ENV || 7000
 
 app.get('/',(request, response, next) => {
     queries.getAll().then(cats => response.json({cats}))
